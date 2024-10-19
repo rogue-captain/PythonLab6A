bb5dc754c00b180d40bdfcae9cfe6cbcf23d8e34
Sully Erkan-Rijos <mr.suleyman.erkan@gmail.com>
2024-10-19 17:45:02 -0400

    first commit

---
+++ ErkanRijosSuleymanLab6A.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 16:34:11 2024
+
+@author: Sully
+"""
+
+##############################################################################
+# Name: Suleyman Erkan-Rijos | CST-121 | Y01
+
+# Program: Lab 6A
+
+# Due Date: 10/26/2024
+#
+# Program Description: 
+    
+#A
+#This program 
+
+# 
+#
+# Inputs: A: 
+#         B: 
+#
+# Outputs: A: 
+#          B: 
+##############################################################################
+
+import statistics #import stats module
+import math # import math module
+
+##################################### 1 #######################################
+
+################################ VARIABLES ####################################
+
+
+
+################################ VARIABLES ####################################
+
+
+def squareFunction(num):
+    
+    for j in range(len(num)):
+        num[j] = num[j]**2
+        
+    return num   
+
+
+        
+def main():
+    
+    list1 = [2, 5, 7, 9, 11, 22]
+    list2 = []
+    
+    for i in range(100, 150, 5):
+        list2 += [i]
+    
+    print(list2)
+
+    combinedList = list1 + list2
+
+    print("Added list:", combinedList )
+    
+    for i in range(len(combinedList)):
+        print(f"{i}{combinedList}")
+        
+    squared = squareFunction(combinedList)
+    print("squared list: ", squared)
+    
+    
+main()
+
+print()
+
+##################################### 1 #######################################
+
+##################################### 2 #######################################   
+def sum_value():
+    
+def collect():
+    
+    num = 0
+     
+    list1 = []
+    
+    while num != -1:
+        
+        num = int(input("enter an integer please. "))
+                
+        list1 += [num]
+        
+        print("the list: ", list1)
+    
+    
+
+
+def main(): 
+    
+    collect()
+    
+
+    
+main() 
+    
+    
